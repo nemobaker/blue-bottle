7b560591ab7d944cece0c95c4b01293b52c3bf53
Nemo Baker <nemobaker@nemos-mbp.lan>
2020-05-18 10:58:20 -0700

    add BrowserStack webdriver boilerplage

---
+++ conf/parallel.conf.js
@@ -0,0 +1,52 @@
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
+
+  updateJob: false,
+  specs: [
+    './tests/specs/single_test.js'
+  ],
+  exclude: [],
+
+  maxInstances: 10,
+  commonCapabilities: {
+    name: 'parallel_test',
+    build: 'webdriver-browserstack'
+  },
+
+  capabilities: [{
+    browser: 'chrome'
+  },{
+    browser: 'firefox'
+  },{
+    browser: 'internet explorer'
+  },{
+    browser: 'safari'
+  }],
+
+  logLevel: 'warn',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  host: 'hub.browserstack.com',
+  
+  before: function () {
+    var chai = require('chai');
+    global.expect = chai.expect;
+    chai.Should();
+  },
+  framework: 'mocha',
+  mochaOpts: {
+      ui: 'bdd'
+  }
+}
+
+// Code to support common capabilities
+exports.config.capabilities.forEach(function(caps){
+  for (var key in caps) { exports.config.commonCapabilities[key] = caps[key]; }
+  return exports.config.commonCapabilities
+});
+
